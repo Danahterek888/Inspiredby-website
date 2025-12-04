af796a7c6dac219d710ac560d8bcc2d1eddde8da
nadeenchami0717-bit <nadeenchami0717@gmail.com>
2025-12-04 18:17:39 +0200

    added paragraph in contactus page

--- src/pages/contactus/Contactus.jsx
+++ src/pages/contactus/Contactus.jsx
@@ -1,7 +1,8 @@
 function Contactus(){
     return(
         <div className="font">
-            meow meow meow meow meow meow
+            We're always happy to hear from you. Whether you have a question about our services, need technical support, or just want to share feedback, <br/>
+            please don't hesitate to reach out. Fill out the form below, and our dedicated team will get back to you as soon as possible, usually within 24 hours
         </div>
     )
 
