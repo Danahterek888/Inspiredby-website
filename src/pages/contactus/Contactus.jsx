18846fa8238f31aa69e7f5a5a0d8be77e0bfad0a
nadeenchami0717-bit <nadeenchami0717@gmail.com>
2025-12-14 20:20:38 +0200

    ci file

--- src/pages/contactus/Contactus.jsx
+++ src/pages/contactus/Contactus.jsx
@@ -2,7 +2,7 @@ function Contactus(){
     return(
         <div className="font">
             We're always happy to hear from you. Whether you have a question about our services, need technical support, or just want to share feedback, <br/>
-            please don't hesitate to reach out. Fill out the form below, meooooooooooow and our dedicated team will get back to you as soon as meoooow possible, usually within 24 hours
+            please don't hesitate to reach out. Fill out the form below, meoooooooooooooow and our dedicated team will get back to you as soon as meoooow possible, usually within 24 hours
         </div>
     )
 
