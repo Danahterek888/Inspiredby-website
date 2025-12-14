6f2424d5c3d24159f3d51794fae1355a8199c966
nadeenchami0717-bit <nadeenchami0717@gmail.com>
2025-12-14 22:25:52 +0200

    fixing

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react'; // ✅ import the plugin
 
 export default defineConfig({
+  plugins: [react()],          // ✅ use the plugin here
   test: {
-    environment: 'jsdom', // <--- enables DOM for tests
+    environment: 'jsdom',      // enables DOM for tests
   },
-   plugins: [react()],
 });
